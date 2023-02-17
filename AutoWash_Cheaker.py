029fb85374618c5081dd64f4fcee3473e693fdbe
l22hs2 <l22hs2@gmail.com>
2023-02-17 20:57:29 +0900

    텔레그램 API 변경, 메시지 가독성 개선 및 링크 버튼 추가 v.1.1

--- AutoWash_Cheaker.py
+++ AutoWash_Cheaker.py
@@ -1,5 +1,10 @@
+import os
 import time
 import datetime
+import requests
+from dotenv import load_dotenv
+
+load_dotenv()
 
 from selenium import webdriver
 from selenium.webdriver.chrome.service import Service
@@ -7,22 +12,9 @@ from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 
-import telegram
-import asyncio
-
-import account
-
 # 시작 시간 출력
 now = datetime.datetime.now().strftime("%Y-%m-%d %H:%M:%S")
 print(f"=== {now} ===")
-
-# 텔레그램 세팅
-token = account.telegram_token
-chat_id = account.telegram_chat_id
-bot = telegram.Bot(token=token)
-
-async def send_message(message):
-    await bot.send_message(chat_id=chat_id, text=f'{message}')
 
 # 크롬 옵션
 options = webdriver.ChromeOptions()
@@ -37,12 +29,22 @@ driver = webdriver.Chrome(service=Service('/usr/lib/chromium-browser/chromedrive
 # 로그인
 driver.get("https://autowash.co.kr/member/login.php")
 driver.implicitly_wait(5)
-driver.find_element(By.ID, 'loginId').send_keys(account.login_id)
-driver.find_element(By.ID, 'loginPwd').send_keys(account.login_pw)
+driver.find_element(By.ID, 'loginId').send_keys(str(os.environ.get('login_id')))
+driver.find_element(By.ID, 'loginPwd').send_keys(str(os.environ.get('login_pw')))
 driver.find_element(By.CLASS_NAME, 'member_login_order_btn').click()
 
 WebDriverWait(driver, 5).until(EC.presence_of_element_located((By.XPATH, '//div[@class="cate_serach"]'))) # 메인 페이지로 복귀했는지 체크 (로그인 성공)
 driver.find_element(By.XPATH, '//span[@class="btn_scroll_top2"]/a').click() # 출석 체크 배너 클릭
+
+# 텔레그램
+token = os.environ.get('telegram_token')
+chat_id = os.environ.get('telegram_chat_id')
+button = {"inline_keyboard" : [[{"text" : "마일리지 적립 내역", "url" : "https://autowash.co.kr/mypage/mileage.php"}]]}
+
+def telegram(msg):
+    data = {"chat_id" : chat_id, "text": msg, "parse_mode": 'markdown', "reply_markup" : button}
+    url = f"https://api.telegram.org/bot{token}/sendMessage?"
+    requests.post(url, json=data)
 
 # 출석 체크
 def attendance():
@@ -68,7 +70,7 @@ def attendance():
             fail_cnt += 1
             fail = f"출석 체크 실패, 재시도... ({fail_cnt}회)"
             print(fail)
-            # asyncio.run(send_message(fail)) # 실패 메시지 전송
+            telegram(fail) # 실패 메시지 전송
 
             # 5회 이상 실패시 중단
             if fail_cnt >= 5:
@@ -82,13 +84,8 @@ def myPage(alert_text):
     print(f"보유 마일리지: {point}원")
 
     # 메시지 전송
-    msg = f'''
-    [ 오토워시 출석 체크 ]
-    {alert_text}
-    - 보유 마일리지: {point}
-    '''
-    msg = msg.lstrip()
-    asyncio.run(send_message(msg))
+    msg = f"\U00002705* 오토워시 출석 체크*\n{alert_text}\n보유 마일리지: {point}원"
+    telegram(msg)
 
 # 출석 체크 함수 호출
 attendance()
